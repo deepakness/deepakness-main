8dda1f6a2acd16f4dc6b7e458cff3c7c6e97d66c
deepakness <deepakkr342@gmail.com>
2021-02-28 20:44:53 +0530

    added Google Analytics from gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -108,7 +108,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // edit below
-        // trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-115535927-1`,
       },
     },
     {
